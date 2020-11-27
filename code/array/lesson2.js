5355433fba6953f6157364d2fd7c070556ba19cb
guojin <jerrychane@126.com>
2020-11-27 21:35:41 +0800

    改写卡牌分组函数

--- code/array/lesson2.js
+++ code/array/lesson2.js
@@ -1,8 +1,19 @@
 // 求两个数的最大公约数
-export default function gcd (a, b) {
-  if (b === 0) {
-    return a
-  } else {
-    return gcd(b, a % b)
+// export default function gcd (a, b) {
+//   if (b === 0) {
+//     return a
+//   } else {
+//     return gcd(b, a % b)
+//   }
+// }
+
+export default  (arr) => {
+  // 求两个数的最大公约数
+  let gcd = (a,b) => {
+    if (b === 0) {
+      return a
+    } else {
+      return gcd(b, a % b)
+    }
   }
 }
--- test/array/lesson2.test.js
+++ test/array/lesson2.test.js
@@ -2,4 +2,6 @@ import gcd from '../../code/array/lesson2'
 
 test('gcd', () => {
   expect(gcd(6, 9)).toBe(3)
+  expect(gcd(2, 8)).toBe(2)
+  // expect(gcd(2, 8)).toBe(3)
 })

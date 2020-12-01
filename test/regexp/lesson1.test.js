66cb8050a8f314974790e5bce14a41a3bd7c4b04
guojin <jerrychane@126.com>
2020-12-01 08:26:53 +0800

    数据结构与算法：正则表达式 - 重复的子字符串

---
+++ test/regexp/lesson1.test.js
@@ -0,0 +1,9 @@
+import subLoopStr from '../../code/regexp/lesson1'
+
+test('subLoopStr', () => {
+  expect(subLoopStr('abab')).toBe(true)
+})
+
+test('subLoopStr2', () => {
+  expect(subLoopStr('abababc')).toBe(false)
+})

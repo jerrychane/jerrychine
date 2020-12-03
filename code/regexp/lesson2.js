d33ac5d0b4a5ad70f22a58d6f951cc316cd03cc7
guojin <jerrychane@126.com>
2020-12-03 08:31:06 +0800

    正则表达式匹配-代码演示-p有模式

--- code/regexp/lesson2.js
+++ code/regexp/lesson2.js
@@ -6,5 +6,19 @@ export default (s, p) => {
       // return s.length ? true : false
       return !s.length
     }
+    // 判断p模式字符串的第一个字符和s字符串的第一个字符是不是匹配
+    let match = false
+    if (s.length > 0 && (p[0] === s[0] || p[0] === '.')) {
+      match = true
+    }
+    // p有模式
+    if (p.length > 1 && p[1] === "*") { 
+      // 第一种情况：s*匹配0个字符
+      // 第二种情况：s*匹配1个字符，递归下去，用来表示s*匹配多个s
+      return isMatch(s, p.slice(2)) || (match && isMatch(s.slice(1), p))
+    } else {
+      // match = true,匹配后一个字符串
+      return match && isMatch(s.slice(1), p.slice(1))
+    }
   }
 }

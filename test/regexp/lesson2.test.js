780772652a35b0c186eb38df3b1d603984233be5
guojin <jerrychane@126.com>
2020-12-03 08:42:40 +0800

    正则表达式匹配-代码演示-测试用例

--- test/regexp/lesson2.test.js
+++ test/regexp/lesson2.test.js
@@ -1,5 +1,21 @@
 import regModeStr from '../../code/regexp/lesson2'
 
-test('regModeStr', () => {
+test('regModeStr1', () => {
   expect(regModeStr('aa', 'a')).toBe(false)
+})
+
+test('regModeStr2', () => {
+  expect(regModeStr('aa', 'a*')).toBe(true)
+})
+
+test('regModeStr3', () => {
+  expect(regModeStr('ab', '.*')).toBe(true)
+})
+
+test('regModeStr4', () => {
+  expect(regModeStr('aab', 'c*a*b')).toBe(true)
+})
+
+test('regModeStr5', () => {
+  expect(regModeStr('mississippi', 'mis*is*p*.')).toBe(false)
 })

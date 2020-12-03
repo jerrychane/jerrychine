ce54dd21291f7c283ee6cc08c0587dac60c3a5c9
guojin <jerrychane@126.com>
2020-12-03 08:20:34 +0800

    正则表达式匹配-代码演示-边界情况

--- code/regexp/lesson2.js
+++ code/regexp/lesson2.js
@@ -1,5 +1,10 @@
+// s为要匹配的字符串，p为匹配模式
 export default (s, p) => {
-  let isMatch = (s,p) => {
-    
+  let isMatch = (s, p) => {
+    // 边界情况，如果s和p都为空，说明处理结束了，返回true,否则返回false
+    if (p.length <= 0) {
+      // return s.length ? true : false
+      return !s.length
+    }
   }
 }

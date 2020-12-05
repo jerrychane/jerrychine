50c6558681167765362bcf1f2adae273974f3cc2
guojin <jerrychane@126.com>
2020-12-05 23:10:29 +0800

    数据结构与算法: 数组中的第 K 个最大元素

--- code/sort/lesson2.js
+++ code/sort/lesson2.js
@@ -1,4 +1,19 @@
+// export default (arr, k) => {
+//   // 降序排列
+//   return arr.sort((a, b) => b - a)[k-1]
+// }
+
 export default (arr, k) => {
-  // 降序排列
-  return arr.sort((a, b) => b - a)[k-1]
+  let len = arr.length - 1
+  for (let i = len, tmp; i > len; i--) {
+    for (let j = 0; j < i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        tmp = arr[j]
+        arr[j] = arr[j + 1]
+        arr[j + 1] = tmp
+      }
+    }
+  }
+  return arr[len - (k - 1)]
 }
+

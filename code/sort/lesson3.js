900dd3bfc0ca1ccc3b210d5e42b166c3dded3ec2
guojin <jerrychane@126.com>
2020-12-09 08:46:27 +0800

    数据结构与算法 - 最大间距

--- code/sort/lesson3.js
+++ code/sort/lesson3.js
@@ -1,18 +1,26 @@
-// 常规做法
+// // 常规做法
+// export default (arr) => {
+//   // 如果数组长度小于2，返回0
+//   if (arr.length < 2) {
+//     return 0
+//   }
+//   // 排序
+//   arr.sort()
+//   // 保存相邻元素的最大差值
+//   let max = 0
+//   for (let i = 0, len = arr.length - 1, tmp; i < len; i++) {
+//     tmp = arr[i + 1] - arr[i]
+//     if (tmp > max) {
+//       max = tmp
+//     }
+//   }
+//   return max
+// }
+
 export default (arr) => {
-  // 如果数组长度小于2，返回0
   if (arr.length < 2) {
     return 0
   }
-  // 排序
-  arr.sort()
-  // 保存相邻元素的最大差值
   let max = 0
-  for (let i = 0, len = arr.length - 1, tmp; i < len; i++) {
-    tmp = arr[i + 1] - arr[i]
-    if (tmp > max) {
-      max = tmp
-    }
-  }
-  return max
+  return 0
 }

34d29c100454f6d27c40756eacc9fe0047fe98d7
guojin <jerrychane@126.com>
2020-12-11 08:46:56 +0800

    数据结构与算法：最大区间

--- code/sort/lesson3.js
+++ code/sort/lesson3.js
@@ -22,5 +22,23 @@ export default (arr) => {
     return 0
   }
   let max = 0
-  return 0
+  let len = arr.length - 1
+  let space
+  // 冒泡排序
+  for (let i = len, tmp; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
+      tmp = arr[j]
+      if (tmp > arr[j + 1]) {
+        arr[j] = arr[j + 1]
+        arr[j + 1] = tmp
+      }
+    }
+    if (i < len) {
+      space = arr[i+1] - arr[i]
+      if (space > max) {
+        max = space
+      }
+    }
+  }
+  return max
 }

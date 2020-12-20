cfd21b52a99be69ed4ea7dab959350d372dd7bc1
guojin <jerrychane@126.com>
2020-12-20 22:02:24 +0800

    栈-棒球比赛代码实操

---
+++ test/stack/lesson1.test.js
@@ -0,0 +1,9 @@
+import baseball from '../../code/stack/lesson1'
+
+test('baseball-1', () => {
+  expect(baseball(["5", "2", "C", "D", "+"])).toBe(30)
+})
+
+test('baseball-2', () => {
+  expect(baseball(["5", "-2", "4", "C", "D", "9", "+", "+"])).toBe(27)
+})

ae4a9cc1229e99d7aae1b25ca39d6c7a04741130
guojin <jerrychane@126.com>
2020-12-25 22:00:13 +0800

    循环队列-代码实操

--- test/queue/lesson1.test.js
+++ test/queue/lesson1.test.js
@@ -8,3 +8,15 @@ test('MyCircularQueque:1', () => {
   expect(queue.Front()).toBe(2)
   expect(queue.Rear()).toBe(1)
 })
+
+test('MyCircularQueque:2', () => {
+  let queue = new MyCircularQueque(4)
+  queue.enQueue(1)
+  queue.enQueue(2)
+  queue.enQueue(3)
+  queue.enQueue(4)
+  expect(queue.isFull()).toBe(true)
+  expect(queue.enQueue(5)).toBe(false)
+  expect(queue.Front()).toBe(1)
+  expect(queue.Rear()).toBe(4)
+})

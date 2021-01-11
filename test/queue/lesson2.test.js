cedc290672d48324c2885c352ebe06dbc87e983e
jerrychane <jerrychane@126.com>
2021-01-11 22:10:02 +0800

    数据结构与算法：任务队列代码实操

--- test/queue/lesson2.test.js
+++ test/queue/lesson2.test.js
@@ -1,22 +1,8 @@
-import MyCircularQueque from '../../code/queue/lesson1'
+import { TestScheduler } from 'jest'
+import leastInterval from '../../code/queue/lesson2'
 
-test('MyCircularQueque:1', () => {
-  let queue = new MyCircularQueque(4)
-  queue.enQueue(2)
-  expect(queue.isFull()).toBe(false)
-  expect(queue.enQueue(1)).toBe(true)
-  expect(queue.Front()).toBe(2)
-  expect(queue.Rear()).toBe(1)
-})
-
-test('MyCircularQueque:2', () => {
-  let queue = new MyCircularQueque(4)
-  queue.enQueue(1)
-  queue.enQueue(2)
-  queue.enQueue(3)
-  queue.enQueue(4)
-  expect(queue.isFull()).toBe(true)
-  expect(queue.enQueue(5)).toBe(false)
-  expect(queue.Front()).toBe(1)
-  expect(queue.Rear()).toBe(4)
+test('leastInterval', () => {
+  expect(leastInterval(["A","A","A","B","B","B"],2)).toBe(8)
+  expect(leastInterval(["A","A","A","A","B","B","B","C","C"],1)).toBe(9)
+  expect(leastInterval(["A","A","B","B","C","C","D","D"],2)).toBe(8)
 })

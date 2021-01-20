83fb9c72d1504bfa24e9fc4b0393068813e71a88
jerrychane <jerrychane@126.com>
2021-01-20 22:04:28 +0800

    质因数是否在指定质因数范围内

--- code/heap/lesson2.js
+++ code/heap/lesson2.js
@@ -1,7 +1,40 @@
 class Ugly {
+  constructor(n, primes) {
+    this.n = n;
+    this.primes = primes;
+  }
+  // 计算超级丑数
+  getAll() {
+    // 超级丑数列表
+    let res = [1];
+    let i = 2;
+    let primes = this.primes;
+    while (res.length < this.n) {
+      let arr = Ugly.getPrimes(i);
+      let k = 0;
+      let l = arr.length;
+      for (; k < l; k++) {
+        if (!primes.find((item) => item === arr[k])) {
+          break;
+        }
+      }
+      // k === l 有两种情况，一种就是当前这个数压根没有质因数；一种是所有质因数都在指定列表中
+      if (k === l) {
+        if (l === 0) {
+          if (primes.find((item) => item === i)) {
+            res.push(i);
+          }
+        } else {
+          res.push(i);
+        }
+      }
+      i++;
+    }
+    return res[this.n - 1];
+  }
   // 计算指定正整数n的质因数
   static getPrimes(n) {
-    // 计算质因数的函数
+    //
     let prime = (n) => {
       // 存储所有的质因数
       let arr = [];

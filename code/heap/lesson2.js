306d082fef85843fc9d02869bdb4e349a8a9a655
jerrychane <jerrychane@126.com>
2021-01-20 22:22:30 +0800

    超级丑数-代码演示(堆查找)

--- code/heap/lesson2.js
+++ code/heap/lesson2.js
@@ -1,7 +1,7 @@
 class Ugly {
   constructor(n, primes) {
     this.n = n;
-    this.primes = primes;
+    this.primes = new Heap(primes);
   }
   // 计算超级丑数
   getAll() {
@@ -14,14 +14,14 @@ class Ugly {
       let k = 0;
       let l = arr.length;
       for (; k < l; k++) {
-        if (!primes.find((item) => item === arr[k])) {
+        if (!primes.find(arr[k])) {
           break;
         }
       }
       // k === l 有两种情况，一种就是当前这个数压根没有质因数；一种是所有质因数都在指定列表中
       if (k === l) {
         if (l === 0) {
-          if (primes.find((item) => item === i)) {
+          if (primes.find(i)) {
             res.push(i);
           }
         } else {
@@ -49,4 +49,68 @@ class Ugly {
   }
 }
 
+class Heap {
+  constructor(arr) {
+    this.data = arr;
+    this.max = arr.length;
+    this.sort();
+  }
+  find(val, i = 0) {
+    let arr = this.data;
+    if (val > arr[i] || i > this.max) {
+      return false;
+    } else if (val === arr[i]) {
+      return val;
+    } else {
+      return this.find(val, i * 2 + 1) || this.find(val, i * 2 + 2);
+    }
+  }
+  sort() {
+    let iArr = this.data;
+    let n = iArr.length;
+    if (n <= 1) {
+      return iArr;
+    } else {
+      for (let i = Math.floor(n / 2); i >= 0; i--) {
+        Heap.maxHeapify(iArr, i, n);
+      }
+      return iArr;
+    }
+  }
+  // 交换两个元素的方法
+  static swap(arr, a, b) {
+    if (a === b) {
+      return "";
+    }
+    let c = arr[a];
+    arr[a] = arr[b];
+    arr[b] = c;
+  }
+  /**
+   * 构建最大堆的过程
+   * @param {*} Arr 数组
+   * @param {*} i  哪个节点
+   * @param {*} size 数组有效长度
+   */
+  static maxHeapify(Arr, i, size) {
+    // 左节点（索引）
+    let l = i * 2 + 1;
+    // 右节点（索引）
+    let r = i * 2 + 2;
+    let largest = i;
+    // 父节点i和左节点l作比较取最大值
+    if (l <= size && Arr[l] > Arr[largest]) {
+      largest = l;
+    }
+    // 右节点和最大值比较
+    if (r <= size && Arr[r] > Arr[largest]) {
+      largest = r;
+    }
+    if (largest !== i) {
+      Heap.swap(Arr, i, largest);
+      Heap.maxHeapify(Arr, largest, size);
+    }
+  }
+}
 export default Ugly;
+export { Heap };
